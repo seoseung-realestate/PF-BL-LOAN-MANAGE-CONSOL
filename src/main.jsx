e81c32260d11c98142833bed15ecccf6d0c6803a
seoseung-realestate <izzang1361@naver.com>
2025-04-20 01:04:12 +0900

    Add files via upload

--- src/main.jsx
+++ src/main.jsx
@@ -1,23 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import AdminPage from "./pages/AdminPage";
-import SignupPage from "./pages/SignupPage";
-
-const AdminStub = () => (
-  <div className="h-screen flex items-center justify-center bg-gray-100 text-xl">
-    관리자 페이지 (추후 구현 예정)
-  </div>
-);
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Router>
+    <BrowserRouter>
       <Routes>
-        <Route path="/" element={<AdminPage />} />
-        <Route path="/signup" element={<SignupPage />} />
-        <Route path="/admin" element={<AdminStub />} />
+        <Route path="/" element={<HomePage />} />
       </Routes>
-    </Router>
+    </BrowserRouter>
   </React.StrictMode>
 );
